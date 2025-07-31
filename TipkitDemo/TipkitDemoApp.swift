66a9f3c84925d102c3780b7a0c29083521b434a6
itsuki <itsuki.enjyoy@gmail.com>
2025-07-31 16:54:03 +0900

    Initial Commit

---
+++ TipkitDemo/TipkitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipkitDemoApp.swift
+//  TipkitDemo
+//
+//  Created by Itsuki on 2025/07/31.
+//
+
+import SwiftUI
+
+@main
+struct TipkitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
